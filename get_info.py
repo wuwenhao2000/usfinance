0b405d8cc49184af66ae836d4367b069b7d4854b
Wenhao Wu <wewu@pivotal.io>
2019-10-03 19:40:38 +0800

    add the description

--- get_info.py
+++ get_info.py
@@ -1,15 +1,21 @@
 import yfinance as yf
-import numpy
+import numpy as np
 import pandas as pd
+import matplotlib
+import seaborn
 import pprint
 import time
 import json
 import os
+
 one = 'SE' # set the corresponding stock
+# variables setting block
 history_file = "{}_{}.xlsx".format(one,time.strftime("%Y%m%d", time.localtime())) # today's history file
 target_file = "{}/{}".format(one,history_file) # today's history file with relative path
 json_file = "{}_{}.json".format(one,time.strftime("%Y%m%d", time.localtime())) # today's variable file
 target_json = "{}/{}".format(one,json_file) # today's variable file with relative path
+
+# get stock information block
 if not os.path.exists(one): # check if the corresponding directory exists
   os.mkdir(one) # if there is NO corresponding directory, then create it
 if not os.path.exists(target_json): # check if the variable file exists
@@ -19,10 +25,12 @@ if not os.path.exists(target_json): # check if the variable file exists
     df = stock.history(period="max") # get the history behavior of the stock
     df.to_excel(target_file) # save the stock history behavior as Excel file
 
-# read the local files and do the analysis with pandas
+# read the local files block
 df = pd.read_excel(target_file) # get the stock history from .xlsx file
 with open(target_json,'r') as load_f: # get the variable from .json file
   curr_var = json.loads(json.dumps(eval(load_f.read()))) # transfer the variable string -> json -> dict
+
+# data analysis block
 # df['eps'] = stock.info['epsTrailingTwelveMonths']
 df['eps'] = curr_var['epsTrailingTwelveMonths'] # get the EPS
 df['trailingPE'] = df['Close']/df['eps'] # calculate new column "trailingPE = Close/eps"
