09dfc0f2798e868bfb528b9bc67ccf0a9d6b9505
Wenhao Wu <wewu@pivotal.io>
2019-10-03 18:31:20 +0800

    merge stock.py into get_info.py

--- get_info.py
+++ get_info.py
@@ -3,25 +3,30 @@ import numpy
 import pandas as pd
 import pprint
 import time
+import json
 import os
-one = 'PVTL' # set the corresponding stock
-stock = yf.Ticker(one) 
-df = stock.history(period="max") # get the history behavior of the stock
-history_file = "{}_{}.xlsx".format(one,time.strftime("%Y%m%d", time.localtime()))
-target_file = "{}/{}".format(one,history_file)
-if not os.path.exists(one): # check if there is the corresponding directory of the stock
+one = 'SE' # set the corresponding stock
+history_file = "{}_{}.xlsx".format(one,time.strftime("%Y%m%d", time.localtime())) # today's history file
+target_file = "{}/{}".format(one,history_file) # today's history file with relative path
+json_file = "{}_{}.json".format(one,time.strftime("%Y%m%d", time.localtime())) # today's variable file
+target_json = "{}/{}".format(one,json_file) # today's variable file with relative path
+if not os.path.exists(one): # check if the corresponding directory exists
   os.mkdir(one) # if there is NO corresponding directory, then create it
-if not os.path.exists(target_file): # check the exist of target file
-  df.to_excel(target_file) # if there is NO target file, then create it
+if not os.path.exists(target_json): # check if the variable file exists
+  stock = yf.Ticker(one) # call yf API
+  with open(target_json,'w') as f: # create json file
+    f.write(str(stock.info)) # save the stock.info as the json file
+    df = stock.history(period="max") # get the history behavior of the stock
+    df.to_excel(target_file) # save the stock history behavior as Excel file
 
-
-df = pd.read_excel(target_file) 
-df['trailingPE'] = df['Close']/stock.info['epsTrailingTwelveMonths']
-df['PE%'] = (df['trailingPE'] - df['trailingPE'].min())/(df['trailingPE'].max()-df['trailingPE'].min())
-df['Regular%'] = (df['Close'] - df['Close'].min())/(df['Close'].max()-df['Close'].min())
-# print (df[df['Date']>time.ctime(stock.info['earningsTimestamp'])][['Date','Close','trailingPE','PE%','Regular%']])
+# read the local files and do the analysis with pandas
+df = pd.read_excel(target_file) # get the stock history from .xlsx file
+with open(target_json,'r') as load_f: # get the variable from .json file
+  curr_var = json.loads(json.dumps(eval(load_f.read()))) # transfer the variable string -> json -> dict
+# df['eps'] = stock.info['epsTrailingTwelveMonths']
+df['eps'] = curr_var['epsTrailingTwelveMonths'] # get the EPS
+df['trailingPE'] = df['Close']/df['eps'] # calculate new column "trailingPE = Close/eps"
+df['PE%'] = (df['trailingPE'] - df['trailingPE'].min())/(df['trailingPE'].max()-df['trailingPE'].min()) # calculate PE%
+df['Regular%'] = (df['Close'] - df['Close'].min())/(df['Close'].max()-df['Close'].min()) # calculate Regular%
+# print (df[df['Date']>time.ctime(stock.info['earningsTimestamp'])][['Date','Close','eps','trailingPE','PE%','Regular%',]])
 print (df[['Date','Close','trailingPE','PE%','Regular%']])
-
-
-
-
--- stock.py
+++ stock.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/Users/wewu/p3/bin/python
 # PE means using all values / earning value
 import yfinance as yf
@@ -6,8 +5,21 @@ import numpy
 import pandas as pd
 import pprint
 import time
-stock = yf.Ticker("PVTL")
-# pprint.pprint (stock.info)
+import os
+import json
+one = "SE"
+json_file = "{}_{}.json".format(one,time.strftime("%Y%m%d", time.localtime()))
+target_json = "{}/{}".format(one,json_file)
+if not os.path.exists(one):
+  os.mkdir(one) 
+if not os.path.exists(target_json): # check the exist of target file
+  stock = yf.Ticker(one)
+  with open(target_json,'w') as f:
+    f.write(str(stock.info))
+with open(target_json,'r') as load_f:
+  curr_var = json.loads(json.dumps(eval(load_f.read())))
+print (curr_var['symbol'])
+  # df.to_excel(target_file) # if there is NO target file, then create it
 # print (stock.info['marketCap'])
 # print (stock.info['sharesOutstanding'])
 # print (stock.info['regularMarketPrice'])
@@ -15,7 +27,7 @@ stock = yf.Ticker("PVTL")
 # print (stock.info['epsForward'])
 # print (stock.info['forwardPE'])
 # # print (stock.info['trailingPE'])
-df = stock.history(period="max")
+# df = stock.history(period="max")
 # print (sample)
 
 # sample.to_excel("SE.xlsx",index=False) # save the df as Excel file without its index written
@@ -34,14 +46,8 @@ df = stock.history(period="max")
 # for key in dict(stock.info):
 #      print("{}, {}".format(key,dict(stock.info)[key]))
 #      # print (key),(dict(stock.info)[key])
-=======
 #!/Users/wenhaowu/p3/bin/python
-import yfinance as yf
-import numpy
-import pandas
-import pprint
 # msft = yf.Ticker("PVTL")
->>>>>>> ee9118e5411d3b7be35d23595ab8cd78909a95a3
 
 # get stock info
 # pprint (msft.info)
@@ -58,19 +64,18 @@ import pprint
 # # show splits
 # print(msft.splits)
 
-<<<<<<< HEAD
+
 # # # show financials
 # print (stock.financials)
 
 # # # show balance heet
 # print (stock.balance_sheet)
-=======
+
 # # show financials
 # print((msft.financials))
 
 # # show balance sheet
 # print(msft.balance_sheet)
->>>>>>> ee9118e5411d3b7be35d23595ab8cd78909a95a3
 
 # # show cashflow
 # print(msft.cashflow)
@@ -79,7 +84,7 @@ import pprint
 # print(msft.options)
 
 # # get option chain for specific expiration
-<<<<<<< HEAD
+
 # opt = stock.option_chain('2019-02-02')
 # print (opt.calls)
 # # data available via: opt.calls, opt.puts
@@ -103,12 +108,9 @@ import pprint
 # print (df[df['Date']>'2018-08-21'])[['Date','Close','trailingPE','PE%','Regular%']]
 
 # print (df)
-df['trailingPE'] = df['Close']/stock.info['epsTrailingTwelveMonths']
-df['PE%'] = (df['trailingPE'] - df['trailingPE'].min())/(df['trailingPE'].max()-df['trailingPE'].min())
-df['Regular%'] = (df['Close'] - df['Close'].min())/(df['Close'].max()-df['Close'].min())
-print (df[df['Date']>'2018-08-21'][['Date','Close','trailingPE','PE%','Regular%']])
-
-=======
+# df['trailingPE'] = df['Close']/stock.info['epsTrailingTwelveMonths']
+# df['PE%'] = (df['trailingPE'] - df['trailingPE'].min())/(df['trailingPE'].max()-df['trailingPE'].min())
+# df['Regular%'] = (df['Close'] - df['Close'].min())/(df['Close'].max()-df['Close'].min())
+# print (df[df['Date']>'2018-08-21'][['Date','Close','trailingPE','PE%','Regular%']])
 # opt = msft.option_chain('YYYY-MM-DD')
 # # data available via: opt.calls, opt.puts
->>>>>>> ee9118e5411d3b7be35d23595ab8cd78909a95a3
