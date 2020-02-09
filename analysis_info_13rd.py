41e8e3341c03410648f83281363fa837203ee3ba
Power <wuwenhao0907@gmail.com>
2020-02-09 19:50:25 +0800

    20200209

--- analysis_info_13rd.py
+++ analysis_info_13rd.py
@@ -15,56 +15,56 @@ from datetime import datetime
 # beginning date of stock dataframe analysis
 date = '2010-01-01'
 
-# SE SNSR SKYY ACWI XSD USD VOX SOCL QTUM
-stock_list = ['se','snsr','ppa','skyy','acwi','xsd','usd','vox','socl','qtum']
+# SE iau SKYY tlt XSD o vnq SOCL QTUM
+stock_list = ['se','iau','ppa','skyy','tlt','xsd','o','vnq','socl','qtum']
 
 # get the stock history from .xlsx file
 df_se = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='se',b=time.strftime("%Y%m%d", time.localtime())))
-df_snsr = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='snsr',b=time.strftime("%Y%m%d", time.localtime())))
+df_iau = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='iau',b=time.strftime("%Y%m%d", time.localtime())))
 df_ppa = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='ppa',b=time.strftime("%Y%m%d", time.localtime())))
 
 df_skyy = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='skyy',b=time.strftime("%Y%m%d", time.localtime())))
-df_acwi = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='acwi',b=time.strftime("%Y%m%d", time.localtime())))
+df_tlt = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='tlt',b=time.strftime("%Y%m%d", time.localtime())))
 df_xsd = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='xsd',b=time.strftime("%Y%m%d", time.localtime())))
 
-df_usd = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='usd',b=time.strftime("%Y%m%d", time.localtime())))
-df_vox = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='vox',b=time.strftime("%Y%m%d", time.localtime())))
+df_o = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='o',b=time.strftime("%Y%m%d", time.localtime())))
+df_vnq = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='vnq',b=time.strftime("%Y%m%d", time.localtime())))
 
 df_socl = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='socl',b=time.strftime("%Y%m%d", time.localtime())))
 df_qtum = pd.read_excel('stock/{a}/{a}_{b}.xlsx'.format(a='qtum',b=time.strftime("%Y%m%d", time.localtime())))
 
 # get the beginning time_line
 df_se = df_se[df_se['Date']>=date]
-df_snsr = df_snsr[df_snsr['Date']>=date]
+df_iau = df_iau[df_iau['Date']>=date]
 df_ppa = df_ppa[df_ppa['Date']>=date]
 
 df_skyy = df_skyy[df_skyy['Date']>=date]
-df_acwi = df_acwi[df_acwi['Date']>=date]
+df_tlt = df_tlt[df_tlt['Date']>=date]
 df_xsd = df_xsd[df_xsd['Date']>=date]
 
-df_usd = df_usd[df_usd['Date']>=date]
-df_vox = df_vox[df_vox['Date']>=date]
+df_o = df_o[df_o['Date']>=date]
+df_vnq = df_vnq[df_vnq['Date']>=date]
 
 df_socl = df_socl[df_socl['Date']>=date]
 df_qtum = df_qtum[df_qtum['Date']>=date]
 
 # set the index of dataframe
 df_se.set_index('Date',inplace=True)
-df_snsr.set_index('Date',inplace=True)
+df_iau.set_index('Date',inplace=True)
 df_ppa.set_index('Date',inplace=True)
 
 df_skyy.set_index('Date',inplace=True)
-df_acwi.set_index('Date',inplace=True)
+df_tlt.set_index('Date',inplace=True)
 df_xsd.set_index('Date',inplace=True)
 
-df_usd.set_index('Date',inplace=True)
-df_vox.set_index('Date',inplace=True)
+df_o.set_index('Date',inplace=True)
+df_vnq.set_index('Date',inplace=True)
 
 df_socl.set_index('Date',inplace=True)
 df_qtum.set_index('Date',inplace=True)
 
 # concat multi dataframe into a new one
-df = pd.concat([df_se,df_snsr,df_ppa,df_skyy,df_acwi,df_xsd,df_usd,df_vox,df_socl,df_qtum],axis=1,keys=stock_list)
+df = pd.concat([df_se,df_iau,df_ppa,df_skyy,df_tlt,df_xsd,df_o,df_vnq,df_socl,df_qtum],axis=1,keys=stock_list)
 
 # set the columns for dataframe
 df.columns.names = ['Stock_Name','Stock_INFO']
@@ -110,15 +110,15 @@ returns = (returns[1:]) # delete the fist row with NaN
 # plt.plot(df.index, df.xs(('VNQ','Close'),axis=1,level=('Stock_Name','Stock_INFO')).rolling(window=30).mean(),label='vnq AVG')
 
 plt.plot(df.index, df.xs(('se','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='se')
-plt.plot(df.index, df.xs(('snsr','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='snsr')
+plt.plot(df.index, df.xs(('iau','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='iau')
 plt.plot(df.index, df.xs(('ppa','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='ppa')
 
 plt.plot(df.index, df.xs(('skyy','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='skyy')
 plt.plot(df.index, df.xs(('xsd','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='xsd')
-plt.plot(df.index, df.xs(('acwi','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='acwi')
+plt.plot(df.index, df.xs(('tlt','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='tlt')
 
-plt.plot(df.index, df.xs(('usd','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='usd')
-plt.plot(df.index, df.xs(('vox','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='vox')
+plt.plot(df.index, df.xs(('o','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='o')
+plt.plot(df.index, df.xs(('vnq','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='vnq')
 
 plt.plot(df.index, df.xs(('socl','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='socl')
 plt.plot(df.index, df.xs(('qtum','Close'),axis=1,level=('Stock_Name','Stock_INFO')),label='qtum')
